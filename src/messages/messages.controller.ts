514c7f26a4a2620af0351968f68128d9b32c3eef
hank <matheus_bueno10@hotmail.com>
2022-07-26 15:42:18 -0300

    finsh first example

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+
+import { MessageDto } from './MessageDto';
+import { MessagesService } from './messages.service';
+
+@Controller('messages')
+export class MessagesController {
+  constructor(private messagesService: MessagesService) {}
+  @Get()
+  findAll() {
+    return this.messagesService.findAll();
+  }
+
+  @Get(':id')
+  findById(@Param() params) {
+    return this.messagesService.findById(+params.id).catch((e) => {
+      throw new NotFoundException(e.message);
+    });
+  }
+  @Post()
+  create(@Body() messageDto: MessageDto) {
+    return this.messagesService.create(messageDto);
+  }
+
+  @Put(':id')
+  update(@Param() params, @Body() messageDto: MessageDto) {
+    return this.messagesService.update(+params.id, messageDto).catch((e) => {
+      throw new NotFoundException(e.message);
+    });
+  }
+
+  @Delete(':id')
+  delete(@Param() params) {
+    return this.messagesService.delete(+params.id).catch((e) => {
+      throw new NotFoundException(e.message);
+    });
+  }
+}
